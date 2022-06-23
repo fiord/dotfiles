3b97c4db1a74772cf9baad1b839f7918692edc2c
fiord <fiord@users.noreply.github.com>
2022-06-23 21:33:57 +0900

    wip

--- link.sh
+++ link.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+cd ~/dotfiles && git submodule update --init --recursive
+cd ~/dotfiles/.vim/dein/repos/github.com/neoclide/coc.nvim && yarn install
+
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.viminfo ~/.viminfo
