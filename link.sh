b742c8b40bd8b7a59623365bf8d607950f4d1995
fiord <hyu.ogawa@dena.com>
2022-08-15 08:50:44 +0900

    fix: link .vim/userautoload

--- link.sh
+++ link.sh
@@ -4,9 +4,10 @@ cd ~/dotfiles/.vim/dein/repos/github.com/neoclide/coc.nvim && yarn install
 
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.vim ~/.vim
+ln -sf ~/dotfiles/.vim/userautoload ~/.vim/userautoload
 ln -sf ~/dotfiles/.viminfo ~/.viminfo
-ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
+# ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+# ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/doftiles/.config ~/.config
 ln -sf ~/dotfiles/.config/nvim ~/.config/nvim
 
